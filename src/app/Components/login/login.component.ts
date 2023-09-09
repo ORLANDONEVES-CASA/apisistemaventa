45023e3b8d311fcf98aef03f7cd7dd9f749d71e9
ORLANDONEVES-CASA <52458831+ORLANDONEVES-CASA@users.noreply.github.com>
2023-09-08 18:08:46 -0600

    actualizaciones

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import { Login } from 'src/app/Interfaces/login';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { UtilidadService } from 'src/app/Reutilizable/utilidad.service';
+import { transition } from '@angular/animations';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
 
+
+export class LoginComponent implements OnInit
+{
+  FormularioLogin:FormGroup;
+  OcultarPassword:boolean=true;
+  MostrarLoading:boolean=false;
+
+  constructor
+  (
+    private fb:FormBuilder,
+    private router:Router,
+    private usuarioServicio: UsuarioService,
+    private utilidadservicio: UtilidadService
+  )
+  {
+    this.FormularioLogin = this.fb.group
+    ({
+      email:["", Validators.required],
+      password:["", Validators.required]
+    });
+  }
+
+  ngOnInit(): void
+  {
+    IniciarSesion()
+    {
+      this.MostrarLoading = true;
+      const request:Login = 
+      {
+        Correo:this.FormularioLogin.value.email,
+        Clave:this.FormularioLogin.value.password
+      }
+      this.usuarioServicio.IniciarSecion(request).subscribe
+      ({
+        next: (data) =>
+        {
+          if(data.status)
+          {
+            this.utilidadservicio.GuardarSesionUsuario(data.value);
+            this.router.navigate(["pages"])
+          }
+          else
+          {
+            this.utilidadservicio.MostrarAlerta("No se puede encontrar resultados","Opps!");
+          }
+        },
+        complete:() =>
+        {
+          this.MostrarLoading=false;
+        },
+        error: ()=>
+        {
+          this.utilidadservicio.MostrarAlerta("Error, hubo problemas al cargar", "Opps!");
+        }
+      })
+    }
+  }
 }
+function IniciarSesion() {
+  throw new Error('Function not implemented.');
+}
+
---
+++ src/app/Reutilizable/utilidad.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Sesion } from '../Interfaces/sesion';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilidadService {
+
+  constructor(private _snackbar:MatSnackBar) { }
+
+  MostrarAlerta(mensaje:string, tipo:string)
+  {
+    this._snackbar.open(mensaje,tipo{
+      horizontalPosition:"end",
+      verticalPosition:"top",
+      duration:3000
+    })
+  }
+
+ GuardarSesionUsuario(UsuarioSesion: Sesion)
+ {
+  localStorage.setItem("usuario", JSON.stringify(UsuarioSesion))
+ }
+
+ ObtenerSesionUsuario()
+ {
+  const dataCadena = localStorage.getItem("usuario")
+  const usuario = JSON.parse(dataCadena!)
+  return usuario;
+ }
+
+ EliminarUsuario()
+ {
+  localStorage.removeItem("usuario")
+ }
+}
